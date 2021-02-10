18927e108667f837d085375a10b82f3ec07cc676
TrcyPrkr <trcyprkr@users.noreply.github.com>
2021-02-09 20:59:23 -0600

    Update WorkingPythonTelegramUploadScript.py

--- WorkingPythonTelegramUploadScript.py
+++ WorkingPythonTelegramUploadScript.py
@@ -3,8 +3,7 @@ import subprocess
 import time
 import shutil
 
-Number_Of_Files=0
-#PATH = r'C:\Users\trcyp\Documents' 
+#Number_Of_Files=0
 PATH = '/home/pi/Public/'
 
 for root, subFolder, files in os.walk(PATH):
@@ -13,11 +12,7 @@ for root, subFolder, files in os.walk(PATH):
     for item in files:
         #Number_Of_Files=Number_Of_Files+1
         fileNamePath = os.path.join(root, item)
-        #sorted = sorted(fileNamePath)
-        #subprocess.run(['telegram-upload', '-f', 'file_dumpster', str(sorted)])
         subprocess.run(['telegram-upload', '-f', 'file_dumpster', str(fileNamePath)])
         os.remove(fileNamePath)
         print(fileNamePath)
-        time.sleep(3)
-    #else:
-        #print(Number_Of_Files)
+        time.sleep(2800)
